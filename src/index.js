052c6cd5df530f7ec46e3f7cb7ed2419a12de4e0
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-10-25 10:13:17 -0400

    Starting output mamagement.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import _ from 'lodash';
 
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import printMe from './print.js';
+
+ function component() {
+   const element = document.createElement('div');
+
+  const btn = document.createElement('button');
+
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+
+  btn.onclick = printMe;
+
+
+  element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
